22ca99fa37cbbceb91f718a12559c78a3c55b8c1
Alexander Popovich <oleshapopovich2000@gmail.com>
2021-05-05 11:02:11 +0300

    Initial commit.

---
+++ src/main/java/com/itechart/orderplanningproblem/OrderPlanningProblemApplication.java
@@ -0,0 +1,13 @@
+package com.itechart.orderplanningproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPlanningProblemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderPlanningProblemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itechart/orderplanningproblem/OrderPlanningProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itechart.orderplanningproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPlanningProblemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
