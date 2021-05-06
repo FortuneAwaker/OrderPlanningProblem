3a03406da3ff99710d31f2051c5801270db0f6d5
Alexander Popovich <oleshapopovich2000@gmail.com>
2021-05-06 11:45:26 +0300

    Created tables and entities.

--- src/main/java/com/itechart/orderplanningproblem/entity/Distance.java
+++ src/main/java/com/itechart/orderplanningproblem/entity/Distance.java
@@ -1,4 +1,35 @@
 package com.itechart.orderplanningproblem.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
 public class Distance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private Double distanceValue;
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+    @ManyToOne
+    @JoinColumn(name = "warehouse_id")
+    private Warehouse warehouse;
+
 }
--- src/main/java/com/itechart/orderplanningproblem/entity/Order.java
+++ src/main/java/com/itechart/orderplanningproblem/entity/Order.java
@@ -1,4 +1,32 @@
 package com.itechart.orderplanningproblem.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private Double amount;
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
 }
